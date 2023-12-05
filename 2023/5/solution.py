745ae49f65ea7dcbf10c2548f198635b16c871ed
Chris Truong <nucat@amazon.com>
2023-12-04 21:42:38 -0800

    Create Template

---
+++ 2023/5/solution.py
@@ -0,0 +1,15 @@
+from input import INPUT_DATA
+
+def solve_part_one():
+    return 0
+
+def solve_part_two():
+    return 0
+
+def main():
+    print(f"Answer 1:     {solve_part_one()}")
+    print(f"Answer 2:     {solve_part_two()}")
+
+
+if __name__ == "__main__":
+    main()
